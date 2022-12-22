8bc9a28202fd357e60aab8c1b75c2c839f44889c
heeeesoooo <118698717+heeeesoooo@users.noreply.github.com>
2022-12-22 17:47:15 +0900

    Add files via upload

--- js/shinhancard.js
+++ js/shinhancard.js
@@ -3,4 +3,22 @@ $(function () {
         console.log($(this).parent())
         $(this).parent().parent().hide()
     })
+
+    $('.icon_box').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 8,
+        slidesToScroll: 8,
+    })
+
+    $('.main_slide .arrows .prev').on('click', function () {
+        $('.icon_box').slick('slickPrev')
+    })
+
+    $('.main_slide .arrows .next').on('click', function () {
+        $('.icon_box').slick('slickNext')
+    })
+
+
+
 })
